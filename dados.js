179d10554f9a8ecaeca7431842e101ded33e54be
jeffersondeoliveira <jeffersondeoliveira004@gmail.com>
2024-09-04 18:01:34 -0300

    Criando arquivos JS

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        name: "Elon Musk",
+        descricion: "Elon Reeve Musk FRS (Pretória, 28 de junho de 1971) é um empreendedor,[5] empresário e filantropo sul-africano-canadense, naturalizado estadunidense. Ele é o fundador, diretor executivo e diretor técnico da SpaceX; CEO da Tesla, Inc.; um dos cofundadores da OpenAI, fundador e CEO da Neuralink",
+        link: "https://pt.wikipedia.org/wiki/Elon_Musk",
+    },  
+    {
+        name: "Pavel Durov", 
+        description: "Pavel Valerievich Durov (Leningrado, 10 de outubro de 1984) é um empresário russo, mais conhecido por ser o fundador do Telegram, um dos aplicativos de mensagens mais populares do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Pavel_Durov"
+    },
+    {
+        name: "Mark Zuckerberg",
+        descriciondes: "Mark Elliot Zuckerberg (White Plains, 14 de maio de 1984) é um programador de computadores e empresário estadunidense. É o co-fundador e CEO do Facebook, uma das maiores redes sociais do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Mark_Zuckerberg"
+    },
+    {
+        name: "ByteDance",
+        descriciondes: "A ByteDance é uma empresa de tecnologia chinesa fundada em 2012. É a empresa-mãe do TikTok, uma das redes sociais de vídeos curtos mais populares do mundo.",
+        link: "https://pt.wikipedia.org/wiki/ByteDance"
+    }
+]
